47a89c2bb4c8eaeff58056ca454fabf3d03d186e
OwenOchieng <owen.ochieng@student.moringaschool.com>
2022-06-13 04:35:02 +0300

    commit tree 02f7a156733526abe62e90b7c19182c46e6d74a1

---
+++ index.js
@@ -0,0 +1,95 @@
+function Monday() {
+    console.log('Go for a five-mile run');
+    console.log('Pump iron');
+  }
+  
+  function Tuesday() {
+    console.log('Go for a five-mile run');
+    console.log('Swim 40 laps');
+  }
+  
+  function Wednesday() {
+    console.log('Go for a five-mile run');
+    console.log('Go for a five-mile run');
+  }
+  
+  function Thursday() {
+    console.log('Go for a five-mile run');
+    console.log('Pump iron');
+  }
+  
+  function Friday() {
+    console.log('Go for a five-mile run');
+    console.log('Swim 40 laps');
+  }
+  
+  function runFiveMiles() {
+    console.log('Go for a five-mile run');
+  }
+
+  function liftWeights() {
+    console.log('Pump iron');
+  }
+  
+  function swimFortyLaps() {
+    console.log('Swim 40 laps');
+  }
+
+  function Monday() {
+    runFiveMiles();
+    liftWeights();
+  }
+
+  function exerciseRoutine(postRunActivity) {
+    runFiveMiles();
+    postRunActivity();
+  }
+
+  function Monday() {
+    exerciseRoutine(liftWeights);
+  }
+
+  exerciseRoutine(() => console.log('Stretch! Work that core!'));
+
+  function morningRoutine(exercise) {
+    let breakfast;
+  
+    if (exercise === liftWeights) {
+      breakfast = 'protein bar';
+    } else if (exercise === swimFortyLaps) {
+      breakfast = 'kale smoothie';
+    } else {
+      breakfast = 'granola';
+    }
+  
+    exerciseRoutine(exercise);
+    return function() {
+        console.log(`Nom nom nom, this ${breakfast} is delicious!`);
+      }
+    }
+
+    const afterExercise = morningRoutine(liftWeights);
+
+    afterExercise;
+
+    afterExercise();
+
+
+    function receivesAFunction(callback) {
+        callback();
+    }
+    
+    
+    
+    function returnsANamedFunction() {
+        function newFunc() {
+            return 1 + 1;
+        }
+        return newFunc;
+    }
+    
+    function returnsAnAnonymousFunction() {
+        return function() {
+            console.log("")
+        }
+    }
